25d7b877f1627c3227cf871fd8d8b0c50fd35aba
kye8524 <cjy7686@naver.com>
2021-11-27 00:41:35 +0900

    [feat] Header 수정

--- FiveFind_front/fivefind/src/components/Header/Header.jsx
+++ FiveFind_front/fivefind/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React, {Component, useState, useEffect} from "react";
+import {Link, withRouter} from "react-router-dom";
 import "./Header.css";
 
 function Header() {
@@ -12,13 +13,14 @@ function Header() {
 
         return(
             <div className={scrollPosition < 300 ? "header" : "nav"}>
-                <div className="header_title">FiveFind</div>
+                <Link to={'/'}><div className="header_title">FiveFind</div></Link>
                 <ul className="header_box">
                     <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}>팀 모집</li>
                     <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}>팀 찾기</li>
                     <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}>공지사항</li>
                     <li className="header_box_bar" />
-                    <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}>로그인 / 회원가입</li>
+                    <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}><Link to='/login'><div>로그인</div></Link></li>
+                    <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}><Link to='/join'><div>회원가입</div></Link></li>
                 </ul>
             </div>
         )
