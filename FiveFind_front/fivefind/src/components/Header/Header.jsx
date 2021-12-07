404a4a460869e692752a7a66baa93b7cfe687afe
yui880 <kimyuna521@gmail.com>
2021-12-08 01:31:39 +0900

    [feat] join, login 완료

--- FiveFind_front/fivefind/src/components/Header/Header.jsx
+++ FiveFind_front/fivefind/src/components/Header/Header.jsx
@@ -13,15 +13,15 @@ function Header() {
         window.addEventListener('scroll', updateScroll);
     });
     return(
-            <div className={scrollPosition < 300 ? "header" : "nav"}>
+            <div className="header">
                 <Link to={'/'}><div className="header_title">FiveFind</div></Link>
                 <ul className="header_box">
-                    <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}>팀 모집</li>
-                    <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}>팀 찾기</li>
-                    <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}>공지사항</li>
+                    <li className="header_box_text">팀 모집</li>
+                    <li className="header_box_text">팀 찾기</li>
+                    <li className="header_box_text">공지사항</li>
                     <li className="header_box_bar" />
-                    <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}><Link to='/login'><div>로그인</div></Link></li>
-                    <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}><Link to='/join'><div>회원가입</div></Link></li>
+                    <li className="header_box_text"><Link to='/login'><div>로그인</div></Link></li>
+                    <li className="header_box_text"><Link to='/join'><div>회원가입</div></Link></li>
                 </ul>
             </div>
         )
