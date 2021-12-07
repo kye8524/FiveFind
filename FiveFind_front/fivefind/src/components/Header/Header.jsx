1557034b717ab8ba46062f433e1502ea91c6c40b
yui880 <kimyuna521@gmail.com>
2021-12-08 00:59:29 +0900

    [feat] 수정 3차

--- FiveFind_front/fivefind/src/components/Header/Header.jsx
+++ FiveFind_front/fivefind/src/components/Header/Header.jsx
@@ -11,14 +11,14 @@ function Header() {
         window.addEventListener('scroll', updateScroll);
     });
     return(
-            <div className={scrollPosition < 300 ? "header" : "nav" || window.location.pathname === '/login' ? "header": "header"}>
+            <div className={scrollPosition < 300 ? "header" : "nav"}>
                 <div className="header_title">FiveFind</div>
                 <ul className="header_box">
-                    <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}><NavLink to = "/">팀 모집</NavLink></li>
+                    <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}>팀 모집</li>
                     <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}>팀 찾기</li>
                     <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}>공지사항</li>
                     <li className="header_box_bar" />
-                    <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}><NavLink to = "/login">로그인 / 회원가입</NavLink></li>
+                    <li className={scrollPosition < 300 ? "header_box_scroll" : "header_box_text"}>로그인 / 회원가입</li>
                 </ul>
             </div>
         )
