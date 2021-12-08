d745a7f1b7440d6f6d6c067e964a3026867627b9
kye8524 <cjy7686@naver.com>
2021-12-08 20:43:12 +0900

    [feat] user repository 구현

---
+++ FiveFInd_Back/src/main/java/fivefind/fivefind/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package fivefind.fivefind.repository;
+
+
+import fivefind.fivefind.entity.Member;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends CrudRepository<Member, Integer> {
+    Optional<Member> findByEmail(String email);
+
+    Optional<Member> findBySeq(int userSeq);
+}
