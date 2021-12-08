94402e6707cbd8048dd7c23959a54e1807625077
yui880 <kimyuna521@gmail.com>
2021-12-08 12:58:06 +0900

    [feat] team list page

---
+++ FiveFind_front/fivefind/src/pages/team_list/team_list.jsx
@@ -0,0 +1,47 @@
+import React, {Component} from "react"
+import {Link, Route, withRouter} from 'react-router-dom';
+import Header from "../../components/Header/Header";
+import SideNav from "../../components/SideNav/SideNav";
+import Footer from "../../components/Footer/Footer";
+import People from "../../img/people.svg";
+import './team_list.css';
+
+class TeamList extends Component{
+    render() {
+        return(
+            <div>
+                <div className="list">
+                    <Header/>
+                    <div className="list_container">
+                        <div className="list_bar"/>
+                        <div className="list_title">팀 찾기</div>
+                        <div className="list_box_container">
+                            <ul className="list_box">
+                                <div className="row">
+                                    <img className="list_box_img" src={People}/>
+                                    <li className="list_box_name">김유나</li>
+                                </div>
+                                <li className="list_box_title">프론트엔드 개발자 모집</li>
+                                <li className="list_box_category">개발자</li>
+                                <li className="list_box_text">가나다라마바사</li>
+                            </ul>
+                            <ul className="list_box">
+                                <div className="row">
+                                    <img className="list_box_img" src={People}/>
+                                    <li className="list_box_name">김유나</li>
+                                </div>
+                                <li className="list_box_title">프론트엔드 개발자 모집</li>
+                                <li className="list_box_category">개발자</li>
+                                <li className="list_box_text">가나다라마바사</li>
+                            </ul>
+
+                        </div>
+                    </div>
+                </div>
+                <Footer/>
+            </div>
+        )
+    }
+}
+
+export default TeamList;
