34e208ad2cb47057e76b66cbc3bcec15a4bc5748
kye8524 <cjy7686@naver.com>
2021-12-15 23:13:57 +0900

    [feat] Front- 팀 등록

--- FiveFind_front/fivefind/src/pages/team_recruit/team_recruit.jsx
+++ FiveFind_front/fivefind/src/pages/team_recruit/team_recruit.jsx
@@ -6,10 +6,63 @@ import Footer from "../../components/Footer/Footer";
 import People from "../../img/people.svg";
 import './team_recruit.css';
 import  '../team_list/team_list.css';
+import axios from "axios";
+import {getCookie, setCookie} from "../../cookies";
 
 
 class TeamRecruit extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            title: '',
+            category: '',
+            content: '',
+            token : undefined,
+            userSeq : undefined
+        }
+    }
+
+    join = () => {
+        let {name, title, category, content} = this.state;
+        if(!name || !title || !category || !content){
+            alert("필수 항목을 작성해주세요")
+        }else {
+            let result = axios({
+                method: 'POST',
+                url: "http://localhost:8080/team/regist",
+                headers: {
+                    "Content-Type": `application/json`,
+                },
+                data: {
+                    name: name,
+                    title: title,
+                    category: category,
+                    content : content
+                }
+            }).then((result) => {
+                    const {history} = this.props;
+                    alert('팀등록이 성공적으로 완료되었습니다.');
+                    history.push('/');
+
+            });
+        }
+    }
+
+    handleChangeTitle = (e) => {
+        this.setState({title: e.target.value})
+    }
+    handleChangeCategory = (e) => {
+        this.setState({category: e.target.value})
+    }
+    handleChangeContent = (e) => {
+        this.setState({content: e.target.value})
+    }
+    handleChangeName = (e) => {
+        this.setState({name: e.target.value})
+    }
     render() {
+        const {name,title,category,content} = this.state;
         return(
             <div>
                 <div className="list">
@@ -20,16 +73,17 @@ class TeamRecruit extends Component{
                             <div className="recruit_container">
                                 <ul className="recruit_box">
                                     <li className="recruit_box_title">팀 이름</li>
-                                    <input type="text" className="recruit_box_text"/>
+                                    <input type="text" className="recruit_box_text" value={name} onChange={this.handleChangeName} required/>
                                 </ul>
                                 <ul className="recruit_box">
                                     <li className="recruit_box_title">제목</li>
-                                    <input type="text" className="recruit_box_text"/>
+                                    <input type="text" className="recruit_box_text" value={title} onChange={this.handleChangeTitle} required/>
                                 </ul>
                                 <ul className="recruit_box">
                                     <li className="recruit_box_title">카테고리</li>
                                     <form>
-                                        <select className="recruit_box_category">
+                                        <select className="recruit_box_category" value={category} onChange={this.handleChangeCategory}>
+                                            <option>카테고리를 선택해 주세요</option>
                                             <option>반려동물</option>
                                             <option>운동</option>
                                             <option>개발</option>
@@ -39,9 +93,9 @@ class TeamRecruit extends Component{
                                 </ul>
                                 <ul className="recruit_box">
                                     <li className="recruit_box_title">팀 상세설명</li>
-                                    <textarea className="recruit_box_textbox"/>
+                                    <textarea className="recruit_box_textbox" value={content} onChange={this.handleChangeContent} required/>
                                 </ul>
-                                <button className="recruit_box_btn">등록</button>
+                                <button className="recruit_box_btn" onClick={this.join}>등록</button>
                             </div>
                     </div>
                 </div>
